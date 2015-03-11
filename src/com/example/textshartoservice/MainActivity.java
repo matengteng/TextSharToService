48d9b97892a8b74e16f4e167cc316739260d22aa
matengteng <ca.mateng@gamil.com>
2015-03-11 10:04:05 +0800

    1

---
+++ src/com/example/textshartoservice/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.textshartoservice;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		//最最最最
+		//--------
+		///sss
+	}
+}
